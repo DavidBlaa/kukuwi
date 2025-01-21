7107f4dab2c7c75e19c53b44e90e15f90dea6d29
dietrjo <dietrich.jonas04@gmail.com>
2025-01-22 00:00:57 +0100

    added other difficulties

--- src/lib/data/MemoryCards.ts
+++ src/lib/data/MemoryCards.ts
@@ -1,26 +1,36 @@
 import type { MemoryCard } from '$lib/types/types';
 
-export const MemoryCardsData:MemoryCard[] = [
-
+export const MemoryCardsData: MemoryCard[] = [
 	{
-		genre:"Jazz",
-		audioPaths:["Drums - Jazz Break 125 bpm.mp3"]
-
+		genre: 'Jazz',
+		audioPaths: ['Drums - Jazz Break 125 bpm.mp3']
 	},
 	{
-		genre:"Funk",
-		audioPaths:["Bs1 Funk Cmaj 124bpm.mp3"]
-
+		genre: 'Funk',
+		audioPaths: ['Bs1 Funk Cmaj 124bpm.mp3']
 	},
 	{
-		genre:"Punk",
-		audioPaths:["Gt1 Punk Emaj 170bpm.mp3"]
-
+		genre: 'Punk',
+		audioPaths: ['Gt1 Punk Emaj 170bpm.mp3']
 	},
 	{
-		genre:"Blues",
-		audioPaths:["Piano Phrase Blues Sky Eb Minor 92bpm.mp3"]
-
+		genre: 'Blues',
+		audioPaths: ['Piano Phrase Blues Sky Eb Minor 92bpm.mp3']
 	},
-
-]
+	{
+		genre: 'African Funk',
+		audioPaths: ['Bs1 AfricanFunk Gm 120bpm.mp3']
+	},
+	{
+		genre: 'Rock',
+		audioPaths: ['Drums - Rock 72 bpm.mp3']
+	},
+	{
+		genre: 'Chill',
+		audioPaths: ['Gt1 Chill Bm 108bpm.mp3']
+	},
+	{
+		genre: 'Piano',
+		audioPaths: ['Piano Phrase Lazy Day IV-I Ab Major 105 bpm.mp3']
+	}
+];
--- src/routes/Genre-Game/+page.svelte
+++ src/routes/Genre-Game/+page.svelte
@@ -1,43 +1,45 @@
 <script lang="ts">
-
-
 	import MemoryCard from '$lib/components/MemoryCard.svelte';
 	import { onMount } from 'svelte';
-	import { MemoryCardsData } from '$lib/data/MemoryCards';
+	import { MemoryCardsData } from '$lib/data/memoryCards';
 	import { shuffleArray } from '$lib/utils/helperfunctions';
 	import { getRandomIndex, getRandomSubset } from '$lib/utils/helperfunctions.js';
 	import GIF from '$lib/components/GIF.svelte';
 	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
+	import { page } from '$app/stores';
+	import { error } from '@sveltejs/kit';
+	import { genreLevels } from '$lib/data/genreLevels';
 
 	let cards: {
 		genre: string;
 		paused: boolean;
-		isRotated: boolean,
-		index: number,
-		audioPath: string,
-		finished: boolean
+		isRotated: boolean;
+		index: number;
+		audioPath: string;
+		finished: boolean;
 	}[] = $state([]);
-
 
 	let lastID: number = $state(-1);
 	let cardTurned: number = $state(0);
 	let genre: string = $state('');
 	let n_cards: number = $state(0);
-	let done:boolean = $state(false);
+	let done: boolean = $state(false);
 	let volume: number = $state(1);
 
-
 	function handleGIFButtonClick() {
-
 		goto(base + '/');
-
 	}
 
 	onMount(() => {
+		const difficultyParam = $page.url.searchParams.get('difficulty');
+		if (difficultyParam === null) {
+			error(404, { message: 'No difficulty defined in search params.' });
+		}
+		const difficulty = parseInt(difficultyParam);
+		const pairs = genreLevels[difficulty - 1].pairs;
 
-		for (const element of getRandomSubset(MemoryCardsData, 4)) {
-
+		for (const element of getRandomSubset(MemoryCardsData, pairs)) {
 			cards.push({
 				index: -1,
 				genre: element.genre,
@@ -54,27 +56,21 @@
 				audioPath: element.audioPaths[getRandomIndex(element.audioPaths)],
 				finished: false
 			});
-
 		}
 		cards = shuffleArray(cards);
 
 		for (const [index, card] of cards.entries()) {
-
 			card.index = index;
-
 		}
 		n_cards = cards.length;
 	});
 
-
 	function handleClick(index: number): void {
-
 		if (cards[index].finished) {
 			return;
 		}
 
 		if (lastID == index) {
-
 			return;
 		}
 
@@ -84,13 +80,10 @@
 				if (!card.finished) {
 					card.isRotated = false;
 				}
-
 			}
 		}
 		for (const card of cards) {
-
 			card.paused = true;
-
 		}
 		cards[index].isRotated = !cards[index].isRotated;
 		cards[index].paused = !cards[index].paused;
@@ -105,51 +98,40 @@
 				cards[index].finished = true;
 				cards[lastID].finished = true;
 				n_cards -= 2;
-
-
 			}
-
 		}
 
-		if(n_cards == 0){
-
+		if (n_cards == 0) {
 			for (const card of cards) {
-
 				card.paused = true;
-
 			}
 
 			done = true;
-
 		}
-
 	}
-
 </script>
 
 <div class="flex h-screen w-screen flex-col items-center justify-center justify-items-center">
-	<div class="grid h-fit w-fit grid-cols-4 gap-5 rounded-xl bg-gray-300 p-12">
+	<div class="grid h-fit w-fit grid-cols-4 gap-5 rounded-xl bg-gray-300 p-8">
 		{#each cards as c}
 			<MemoryCard
-
 				isRotated={c.isRotated}
 				paused={c.paused}
 				onmouseup={() => handleClick(c.index)}
-				audioSource={"/audios/"+c.audioPath}
+				audioSource={'/audios/' + c.audioPath}
 				finished={c.finished}
-
 			/>
 		{/each}
 	</div>
-		{#if done}
-		<div class="absolute top-0 left-0">
+	{#if done}
+		<div class="absolute left-0 top-0">
 			<GIF
 				bind:success={done}
 				onclick={handleGIFButtonClick}
-				GIFButtonTextSuccess = {"Gewonnen"}
-				GIFButtonTextFailure = {" "}
-				gifSoundPaused = {false}
-				volume = {volume}
+				GIFButtonTextSuccess={'Gewonnen'}
+				GIFButtonTextFailure={' '}
+				gifSoundPaused={false}
+				{volume}
 			/>
 		</div>
 	{/if}
